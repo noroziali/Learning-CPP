5993f5e98581a4d55933b2af93ee6e128305a4cc
Soheyl Norozi <Soheylnorozi@outlook.com>
2019-04-20 21:03:15 +0430

    Syntax erorrs debuged

--- HelloWorld/HelloWorld.cpp
+++ HelloWorld/HelloWorld.cpp
@@ -1,13 +1,12 @@
 // HelloWorld.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
-
 #include <iostream>
 #include <limits>
-int main{
+
+int main()
+{
 	std::cout << "Hello World !";
 
-
 	system("pause");
-}
 	return 0;
 }

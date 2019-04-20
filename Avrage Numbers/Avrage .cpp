9d1f390900d70bd447001c2d2740336a1725c66f
Soheyl Norozi <Soheylnorozi@outlook.com>
2019-04-20 20:46:17 +0430

    Add project files.

---
+++ Avrage Numbers/Avrage .cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <limits>
+
+int main()
+{
+	int a, b, s;
+	std::cout << "enter a :";
+	std::cin >> a;
+	std::cout << "enter b :";
+	std::cin >> b;
+	s = (a + b) / 2;
+	std::cout << s;
+	std::cout << "\nThis Program is Bulid by Soheyl Norozi on 25th of Farvardin 1398 \n";
+	std::cout << "\n\n\nThanks For your Attentions!\n";
+
+	system("pause");
+	return 0;
+}
---
+++ HelloWorld/HelloWorld.cpp
@@ -0,0 +1,13 @@
+// HelloWorld.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <limits>
+int main{
+	std::cout << "Hello World !";
+
+
+	system("pause");
+}
+	return 0;
+}

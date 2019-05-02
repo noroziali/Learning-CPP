556db3b38e42fc61a5169e42b6747bd4a3e6cdfd
Soheyl Norozi <Soheylnorozi@outlook.com>
2019-05-02 17:44:41 +0430

    swich case implemented

--- Swich Cases/Swich Cases.cpp
+++ Swich Cases/Swich Cases.cpp
@@ -1,20 +1,60 @@
-// Swich Cases.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
 
 #include <iostream>
+#include <time.h>
+#include <stdlib.h>
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n"; 
+	srand(time(0));
+	int chose,n=0;
+	char temp;
+	cout << "Hello!\n";
+	cout << "please chose your account type:\n\n";
+	cout << "1.user\n";
+	cout << "2.administrator\n";
+	cout << "3.Manager\n";
+	cout << "4.client admin\n\n";
+	cout << "==========\n";
+	cout << "insert only option number:";
+	cin >> temp;
+	/*while (temp != '4' || temp != '3' || temp != '2' || temp != '1')
+	{
+		cout << "invalid choice, try Again:" << endl;
+		cin >> temp;
+	}*/
+	chose = temp;
+	cout << chose;
+	while (1)
+	{
+		switch (n)
+		{
+		default:
+			break;
+		case 1:
+	
+			break;
+		case 2:
+			break;
+		case 3:
+			break;
+		case 4:
+			break;
+	
+		}
+		cout << "\nThis Program is Bulid by Soheyl Norozi on 3th of Farvardin 1398 \n";
+		cout << "\n\n\nThanks For your Attentions!\n";
+		//system("pause");
+		//system("cls");
+		//cout << "Hello!\n";
+		//cout << "please chose your account type:\n\n";
+		//cout << "1.user\n";
+		//cout << "2.administrator\n";
+		//cout << "3.Manager\n";
+		//cout << "4.client admin\n\n";
+		//cout << "==========\n";
+		//cout << "insert only option number:";
+	}
+	
+	return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file

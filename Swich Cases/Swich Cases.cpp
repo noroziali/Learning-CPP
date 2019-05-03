37e6de7c304e4535c10c1e804e96815c57aa9c0b
Soheyl Norozi <Soheylnorozi@outlook.com>
2019-05-03 14:43:29 +0430

    swich cases comoleted i'm wondering if there was any way to convert ascii code back to number in my codes

--- Swich Cases/Swich Cases.cpp
+++ Swich Cases/Swich Cases.cpp
@@ -7,7 +7,7 @@ using namespace std;
 int main()
 {
 	srand(time(0));
-	int chose,n=0;
+	int n=0;
 	char temp;
 	cout << "Hello!\n";
 	cout << "please chose your account type:\n\n";
@@ -17,43 +17,44 @@ int main()
 	cout << "4.client admin\n\n";
 	cout << "==========\n";
 	cout << "insert only option number:";
-	cin >> temp;
-	/*while (temp != '4' || temp != '3' || temp != '2' || temp != '1')
+	select:cin >> temp;
+	while (temp != '4' && temp != '3' && temp != '2' && temp != '1')
 	{
 		cout << "invalid choice, try Again:" << endl;
 		cin >> temp;
-	}*/
-	chose = temp;
-	cout << chose;
+	}
+	n = temp;
 	while (1)
 	{
 		switch (n)
 		{
-		default:
+		case 49:
+			cout << "you have chosed the first option";
 			break;
-		case 1:
-	
+		case 50:
+			cout << "you have chosed the 2nd option";
 			break;
-		case 2:
+		case 51:
+			cout << "you have chosed the 3th option";
 			break;
-		case 3:
-			break;
-		case 4:
+		case 52:
+			cout << "you have chosed the 4th option";
 			break;
 	
 		}
 		cout << "\nThis Program is Bulid by Soheyl Norozi on 3th of Farvardin 1398 \n";
 		cout << "\n\n\nThanks For your Attentions!\n";
-		//system("pause");
-		//system("cls");
-		//cout << "Hello!\n";
-		//cout << "please chose your account type:\n\n";
-		//cout << "1.user\n";
-		//cout << "2.administrator\n";
-		//cout << "3.Manager\n";
-		//cout << "4.client admin\n\n";
-		//cout << "==========\n";
-		//cout << "insert only option number:";
+		system("pause");
+		system("cls");
+		cout << "Hello!\n";
+		cout << "please chose your account type:\n\n";
+		cout << "1.user\n";
+		cout << "2.administrator\n";
+		cout << "3.Manager\n";
+		cout << "4.client admin\n\n";
+		cout << "==========\n";
+		cout << "insert only option number:";
+		goto select;
 	}
 	
 	return 0;

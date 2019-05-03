db1a82711e74d3e404e68d16027cd91aa336dbb4
Soheyl Norozi <Soheylnorozi@outlook.com>
2019-05-03 14:47:58 +0430

    title added

--- Swich Cases/Swich Cases.cpp
+++ Swich Cases/Swich Cases.cpp
@@ -1,12 +1,11 @@
 
 #include <iostream>
-#include <time.h>
 #include <stdlib.h>
 using namespace std;
 
 int main()
 {
-	srand(time(0));
+	system("title portal");
 	int n=0;
 	char temp;
 	cout << "Hello!\n";

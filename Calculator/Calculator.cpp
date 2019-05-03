a37e48f3c5dd814ede84709378b2479cb44ff64d
Soheyl Norozi <Soheylnorozi@outlook.com>
2019-05-03 14:26:07 +0430

    calculator added swich cases updated

--- Calculator/Calculator.cpp
+++ Calculator/Calculator.cpp
@@ -1,20 +1,56 @@
-// Calculator.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <stdlib.h>
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n"; 
+	int a, b,r;
+	char op;
+	while (1)
+	{
+		cout << "insert your first number:" << endl;
+		cin >> a;
+		cout<<"insert your other number:"<< endl;
+		cin >> b;
+		cout << "pleae select your operation : + or - or * or /" << endl;
+		cin >> op;
+		while (op != '+' && op != '-' && op != '/' && op != '*')
+		{
+			cout << "invalid Operation, Try a Valid Operator:" << endl;
+				cin >> op;
+		}
+		if (op =='+')
+		{
+			cout << "you chosed to have the sum:" << endl;
+			r = a + b;
+			cout << "the result is :" << r << endl;
+
+		}
+		else if (op == '-')
+		{
+			cout << "you chosed to have the minus:" << endl;
+			r = a - b;
+			cout <<"the result is :"<< r << endl;
+		}
+		else if (op == '*')
+		{
+			cout << "you chosed to have the Multipliy:" << endl;
+			r = a * b;
+			cout << "the result is :" << r << endl;
+		}
+		else if (op == '/')
+		{
+			cout << "you chosed to have the substract:" << endl;
+			r = a / b;
+			cout << "the result is :" << r << endl;
+		}
+
+		cout << "\nThis Program is Bulid by Soheyl Norozi on 3th of Farvardin 1398 \n";
+		cout << "\n\n\nThanks For your Attentions!\n";
+		system("pause");
+		system("cls");
+
+	}
+
+	return (0);
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
